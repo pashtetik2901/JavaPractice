7f7e509f15bd3d9f58652dd1a26f864fbc60a26e
pashtetik2901 <gordeevk108@gmail.com>
2025-10-22 11:19:27 +0300

    add_practice_13

--- practice_13/src/Main.java
+++ practice_13/src/Main.java
@@ -1,10 +1,6 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        int code = new Integer("25");
-        System.out.println(code);
-        code = code - 1;
-        System.out.println(code);
+        PhoneNumber number = new PhoneNumber("89207807591");
+        System.out.println(number.getRightNumber());
     }
 }
--- practice_13/src/PhoneNumber.java
+++ practice_13/src/PhoneNumber.java
@@ -2,26 +2,53 @@ public class PhoneNumber {
     private String first_string;
     private String right_string;
 
-    public  PhoneNumber(String str){
+    public PhoneNumber(String str) {
         this.first_string = str;
-        this.right_string = "";
+        this.right_string = convert(first_string);
     }
 
-    private void convert(){
+    private String convert(String first_string) {
         StringBuilder builder = new StringBuilder();
-        if (first_string.charAt(0) == '+') {
-            String code = new String(first_string.substring(2));
+        if (this.first_string.charAt(0) == '+') {
+            String code = new String(this.first_string.substring(0, 2));
             builder.append(code);
-        }
-        else{
-            int code = new Integer(first_string.charAt(0));
-            code = code - 1;
+            builder.append('(' +
+                    this.first_string.substring(2, 5) +
+                    ")-" +
+                    this.first_string.substring(5, 8) +
+                    '-' +
+                    this.first_string.substring(8, this.first_string.length())
+            );
+        } else {
+            int code = Character.getNumericValue(first_string.charAt(0)) - 1;
             builder.append('+');
             builder.append(code);
+            builder.append('(' +
+                    this.first_string.substring(1, 4) +
+                    ")-" +
+                    this.first_string.substring(4, 7) +
+                    '-' +
+                    this.first_string.substring(7, this.first_string.length())
+            );
         }
 
-        builder.append('(' + )
+
+        return builder.toString();
     }
 
+    public String getFirstNumber() {
+        return first_string;
+    }
 
+    public void setFirstNumber(String first_string) {
+        this.first_string = first_string;
+    }
+
+    public String getRightNumber() {
+        return right_string;
+    }
+
+    public void setRightNumber(String right_string) {
+        this.right_string = right_string;
+    }
 }
